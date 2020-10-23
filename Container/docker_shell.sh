cf51ec13975c48136b20066e0a69c4674f91e510
GallonSong <g478551654@gmail.com>
2020-10-23 10:58:21 +0000

    remove unecessary code

--- Container/docker_shell.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker run -td -v $(pwd):/home/fenics/shared -w /home/fenics/shared quay.io/fenicsproject/stable:current
-docker ps -q > docker_id.txt
-docker cp murtazo.tgz $(cat docker_id.txt):/home/fenics/shared/.
-docker cp commands_container.sh $(cat docker_id.txt):/home/fenics/shared/.
-docker cp runme.sh $(cat docker_id.txt):/home/fenics/shared/.
-docker exec $(cat docker_id.txt) /home/fenics/shared/commands_container.sh
